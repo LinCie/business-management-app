e84f2b890f598e736b4ff910b09d3afdd9031bd2
LinCie <aldiofernanda@protonmail.com>
2024-11-29 20:59:12 +0700

    docs: add jsdoc for cn function

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
+/**
+ * A drop-in replacement for `clsx` that supports Tailwind's merge utility.
+ *
+ * @param inputs - A list of class names to merge.
+ * @returns A single class name string.
+ *
+ * @example
+ * import { cn } from "@/lib/utils"
+ *
+ * const Example = () => <div className={cn("bg-red-500", "h-96")}>Hello!</div>
+ *
+ * @see https://github.com/tailwindlabs/tailwindcss/blob/3.x/src/plugins/merge.ts
+ * @see https://github.com/lukeed/clsx
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
